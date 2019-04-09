c85a4c1faceea8b7deb693d0e510e312838226ca
USER <user@user-no-macbook-pro.local>
2019-04-09 20:34:08 +0900

    Initial Commit

---
+++ Listening App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Listening App
+//
+//  Created by USER on 2019/04/09.
+//  Copyright © 2019年 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
