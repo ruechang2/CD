eed3307a986605b2431a091cc56a32fd4295dd0d
USER <user@user-no-macbook-pro.local>
2019-04-20 13:48:07 +0900

    label

--- Listening App/YourViewController.swift
+++ Listening App/YourViewController.swift
@@ -1,40 +1,51 @@
-//
-//  YourViewController.swift
-//  Listening App
-//
-//  Created by USER on 2019/04/17.
-//  Copyright © 2019年 USER. All rights reserved.
-//
-
 import UIKit
-import AVFoundation
+import youtube_ios_player_helper
 
-
-class YourViewController: UIViewController {
+class YourViewController: UIViewController, YTPlayerViewDelegate {
+    @IBOutlet weak var playerView: YTPlayerView!
+    @IBOutlet weak var stateLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 動画ファイルのURLを取得
-        let url = URL(string: "https://www.youtube.com/watch?v=FiHSfk8veTk&t=1s")
-        
-        // 生成
-        let player = AVPlayer(url: url!)
-        
-        // レイヤーの追加
-        let playerLayer = AVPlayerLayer(player: player)
-        playerLayer.frame = self.view.bounds
-        self.view.layer.addSublayer(playerLayer)
-        
-        // 再生
-        player.play()
+        playerView.delegate = self;
+        self.playerView.load(withVideoId: "FiHSfk8veTk", playerVars: ["playsinline":1])
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - IBAction
+    @IBAction func tapPlay(_ sender: Any) {
+   self.playerView.playVideo()
+    }
+
+    @IBAction func tapPause(_ sender: Any) {
+   self.playerView.pauseVideo()
+    }
+
+    @IBAction func tapStop(_ sender: Any) {
+     self.playerView.stopVideo()
+        
+    }
+
+    
+    // MARK: - delegate methods
+    func playerView(_ playerView: YTPlayerView!, didChangeTo state: YTPlayerState) {
+        switch (state) {
+        case YTPlayerState.unstarted:
+            stateLabel.text = "停止中"
+        case YTPlayerState.playing:
+            stateLabel.text = "再生中"
+        case YTPlayerState.paused:
+            stateLabel.text = "一時停止中"
+        case YTPlayerState.buffering:
+            stateLabel.text = "バッファリング中"
+        case YTPlayerState.ended:
+            stateLabel.text = "再生終了"
+        default:
+            break
+        }
     }
 }
-
-
-
